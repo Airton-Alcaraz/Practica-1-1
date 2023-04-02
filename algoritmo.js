190ddee671fc71f54e22c555a30bcc5f3ca5f064
Airton Alcaraz <airton.alcaraz94@gmail.com>
2023-04-02 15:39:43 -0300

    Practica 1-1

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+document.querySelector("body").innerHTML = `
+<h1>Titulo de laPractica 1-1</h1>
+    <h2>Titulo 2</h2>
+    <h3>Titulo 3</h3>
+
+    <img src="https://www.zooplus.es/magazine/wp-content/uploads/2022/05/Cuanto-pesa-un-gato-2.jpeg" alt="gatito-2">
+
+    <img src="Imagenes/gatito-1.jpeg" alt="gatito-1">
+`; 
